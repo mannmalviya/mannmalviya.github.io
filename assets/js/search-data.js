f1931080643180b0a9a3fd88f52ac318293b024a
mannmalviya <31766230+mannmalviya@users.noreply.github.com>
2025-06-26 07:11:02 +0000

    Deploying to gh-pages from @ mannmalviya/mannmalviya.github.io@3f7693406b610ae2c10c297ccbf269e5b3884fc9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,56 +7,56 @@ ninja.data = [{
     title: "Home",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/mannmalviya.github.io/";
+      window.location.href = "/";
     },
   },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/mannmalviya.github.io/projects/";
+            window.location.href = "/projects/";
           },
         },{id: "nav-notes",
           title: "Notes",
           description: "A Collection of Notes I have made in the courses I took at UCSC and from other sources.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/mannmalviya.github.io/Notes/";
+            window.location.href = "/Notes/";
           },
         },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/mannmalviya.github.io/blog/";
+            window.location.href = "/blog/";
           },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/mannmalviya.github.io/CV/";
+            window.location.href = "/CV/";
           },
         },{id: "dropdown-books",
               title: "Books",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/mannmalviya.github.io/books/";
+                window.location.href = "/books/";
               },
             },{id: "dropdown-quotes",
               title: "Quotes",
               description: "",
               section: "Dropdown",
               handler: () => {
-                window.location.href = "/mannmalviya.github.io/Quotes/";
+                window.location.href = "/Quotes/";
               },
             },{id: "nav-aboutme",
           title: "AboutMe",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/mannmalviya.github.io/people/";
+            window.location.href = "/people/";
           },
         },{id: "post-a-post-with-plotly-js",
         
@@ -66,7 +66,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/mannmalviya.github.io/blog/2025/plotly/";
+            window.location.href = "/blog/2025/plotly/";
           
         },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
@@ -95,7 +95,7 @@ ninja.data = [{
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/mannmalviya.github.io/books/the_godfather/";
+              window.location.href = "/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -103,7 +103,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/mannmalviya.github.io/news/announcement_2/";
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -111,47 +111,47 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/1_project/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/2_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/3_project/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/4_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/5_project/";
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/6_project/";
+              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/7_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/8_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/mannmalviya.github.io/projects/9_project/";
+              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
@@ -171,7 +171,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/mannmalviya.github.io//feed.xml", "_blank");
+          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
